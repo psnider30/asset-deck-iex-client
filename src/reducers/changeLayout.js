9845adadfdf055ffa9fc89de6234b5a2214c3c61
psnider30 <pbsnider@gmail.com>
2018-02-23 10:51:24 -0800

    Set asset in redux state back to null in changeLayout reducer if layout is not timeSeries

--- src/reducers/changeLayout.js
+++ src/reducers/changeLayout.js
@@ -4,13 +4,13 @@ export default function changeLayout(state = {
 }, action) {
   switch(action.type) {
     case "MAIN_LAYOUT":
-      return {...state, layout: action.layout}
+      return {...state, layout: action.layout, asset: null}
     case "FUNDAMENTALS_LAYOUT":
-      return {...state, layout: action.layout}
+      return {...state, layout: action.layout, asset: null}
     case "CHANGE_SUMMARY_LAYOUT":
-      return {...state, layout: action.layout}
+      return {...state, layout: action.layout, asset: null}
     case "FINANCIALS_LAYOUT":
-      return {...state, layout: action.layout}
+      return {...state, layout: action.layout, asset: null}
     case "TIME_SERIES_LAYOUT":
       return {...state, layout: action.layout, asset: action.asset}
     default:
