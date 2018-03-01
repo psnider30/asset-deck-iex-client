09741710cb6290ffc348cecb16a725b0591a9aba
psnider30 <pbsnider@gmail.com>
2018-03-01 15:47:37 -0800

    Add redux-react-session and add the sessionReducer to the store

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,3 +1,5 @@
+import { sessionService } from 'redux-react-session';
+
 export function login(data) {
   return {
     type: "LOGIN",
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 // import fetchingData from '../reducers/fetchingDataReducer.js'
 import changeLayout from '../reducers/changeLayout';
 import manageAssets from '../reducers/manageAssets';
+import { sessionService, sessionReducer } from 'redux-react-session';
 // import assetData from '../reducers/assetDataReducer';
 import { users } from '../reducers/usersReducer';
 const middlewares = [thunk];
@@ -10,6 +11,7 @@ const reducers = combineReducers({
   manageAssets,
   changeLayout,
   users,
+  sessionReducer,
 })
 
 export default createStore (
