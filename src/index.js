2c421a3b1c003e562f53201737cdd17066efa2ed
psnider30 <pbsnider@gmail.com>
2018-02-28 17:27:58 -0800

    Remove unnecessary code from Index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
-// import rootReducer from './reducers/rootReducer.js'
 import WebFont from 'webfontloader';
 import store from './store'
 
@@ -14,13 +12,6 @@ WebFont.load({
     families: ['Roboto Slab', 'Mukta Mahee', 'Quicksand']
   }
 });
-
-// wrap store in a function for testing purposes
-// export function configureStore(){
-//   return createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ &&
-//     window.__REDUX_DEVTOOLS_EXTENSION__());
-// };
- // const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
