3888eb706a7ffc847af60e5dd4ec0b65c17f0301
psnider30 <pbsnider@gmail.com>
2018-03-04 20:35:59 -0800

    Add AssetApi with getAllAssets()

---
+++ src/api/assetApi.js
@@ -0,0 +1,19 @@
+class AssetApi {
+  static requestHeaders() {
+   return {'AUTHORIZATION': `Bearer ${sessionStorage.jwt}`}
+ }
+
+ static getAllAssets() {
+   const headers = this.requestHeaders();
+   const request = new Request('http://localhost:3001/api/assets', {
+     method: 'GET',
+     headers: headers
+   });
+
+   return fetch(request).then(response => {
+     return response.json();
+   }).catch(error => {
+     return error;
+   });
+ }
+}
