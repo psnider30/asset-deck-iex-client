078bad07afad07ec416bf4b8f4cbe8bd7be457c8
psnider30 <pbsnider@gmail.com>
2018-04-02 15:04:23 -0700

    try removing process.env from REACT_APP_API_HOST_HEROKU in SessionApi and SignUpApi

--- src/api/signUpApi.js
+++ src/api/signUpApi.js
@@ -1,7 +1,7 @@
 export default class SignUpApi {
 
   static signup(userInfo) {
-    const API_HOST = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_HOST_LOCAL : process.env.REACT_APP_API_HOST_HEROKU;
+    const API_HOST = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_HOST_LOCAL : REACT_APP_API_HOST_HEROKU;
     const request = new Request(`${API_HOST}/signup`, {
       method: 'POST',
       headers: new Headers({
