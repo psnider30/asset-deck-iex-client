5a79b404aac4ddd8420f5653a93fbdc2f7b6a34f
psnider30 <pbsnider@gmail.com>
2018-04-02 17:32:26 -0700

    upadate

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import WebFont from 'webfontloader';
 import store from './store';
-require('./assets/media/favicon.ico');
+import favicon from './assets/media/favicon.ico';
 
 WebFont.load({
   google: {
